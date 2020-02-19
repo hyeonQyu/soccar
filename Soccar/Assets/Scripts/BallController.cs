a02ccbcf97dccc819643f08e58d520486cacc9bc
hyeonQyu <dhk0561@naver.com>
2020-02-19 14:15:11 +0900

    Set the bounciness of ball

--- Soccar/Assets/Scripts/BallController.cs
+++ Soccar/Assets/Scripts/BallController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+    private Collider _collider;
+    private bool _isOnNet;
     private bool _isScored;
     public bool IsScored
     {
@@ -20,12 +22,62 @@ public class BallController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("공 ID: " + gameObject.GetInstanceID());
+        _collider = GetComponent<Collider>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.gameObject.CompareTag("Net"))
+        {
+            Debug.Log("bounce " + _collider.material.bounciness);
+            _collider.material.bounciness *= 0.01f;
+        }
+    }
+
+    // 실제 축구공처럼 튀기게 하기 위함
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 땅에 부딪히면 탄성 감소
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            _collider.material.bounciness *= 0.8f;
+            return;
+        }
+        // 그물은 공을 튀기지 않기 때문에 그물 위에 올라가면 공을 내려줘야 함
+        else if(collision.gameObject.CompareTag("Net"))
+        {
+            _collider.material.bounciness *= 0.01f;
+            _isOnNet = true;
+            StartCoroutine(MoveBallOnNet());
+            return;
+        }
+        // 다른 물체에 부딪히면 탄성을 원래대로
+        _collider.material.bounciness = 0.8f;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Net"))
+        {
+            _isOnNet = false;
+        }
+    }
+
+    private IEnumerator MoveBallOnNet()
+    {
+        int loop = 0;
+        // 4초간 대기하며 공이 그물 위에 있는지 검사
+        while(_isOnNet && loop < 8)
+        {
+            yield return new WaitForSeconds(0.5f);
+            loop++;
+        }
+
+        // 공이 4초 후에도 그물 위에 있으면 탄성 복구 후 랜덤 위치로 공 이동
+        if(_isOnNet)
+        {
+            _collider.material.bounciness = 0.8f;
+            transform.position = new Vector3(Random.Range(-25f, 25f), 30, Random.Range(-25f, 2f));
+        }
     }
 }
--- Soccar/Assets/Scripts/GoalChecker.cs
+++ Soccar/Assets/Scripts/GoalChecker.cs
@@ -6,7 +6,7 @@ public class GoalChecker : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Ball")
+        if(other.gameObject.CompareTag("Ball"))
         {
             GameObject ball = other.gameObject;
             // 해당 공이 득점 상태가 아닌 경우에만 득점 인정(공이 트리거를 한 번에 두 번 통과하는 경우 방지)
@@ -23,9 +23,11 @@ public class GoalChecker : MonoBehaviour
 
     private IEnumerator MoveBall(GameObject ball)
     {
-        yield return new WaitForSeconds(3.0f);
-        // (-25, 20, -25) ~ (25, 20, 25)의 랜덤 위치로 공을 이동시킴
-        ball.transform.position = new Vector3(Random.Range(-25f, 25f), 20, Random.Range(-25f, 2f));
+        yield return new WaitForSeconds(3);
+        // (-25, 30, -25) ~ (25, 30, 25)의 랜덤 위치로 공을 이동시킴
+        ball.transform.position = new Vector3(Random.Range(-25f, 25f), 30, Random.Range(-25f, 2f));
         ball.GetComponent<BallController>().IsScored = false;
+        // 탄성력 복구
+        ball.GetComponent<Collider>().material.bounciness = 0.8f;
     }
 }
