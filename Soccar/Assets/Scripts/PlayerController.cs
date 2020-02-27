f4c13636bb6845c5041b8440df2106da0b9911ba
hyeonQyu <dhk0561@naver.com>
2020-02-27 12:42:16 +0900

    Eidt variables' name

--- Soccar/Assets/Scripts/PlayerController.cs
+++ Soccar/Assets/Scripts/PlayerController.cs
@@ -65,6 +65,9 @@ public class PlayerController : MonoBehaviour
     private Socket _socket = null;
     private const int GameStartPacket = 2015;
     private const int RequestPlayerIndex = 8282;
+    private const string IP = "127.0.0.1";
+    private const int Port = 6666;
+    private const int UninitializedPlayerIndex = 9;
 
 
     SyncInformation _sendPosition;
@@ -75,9 +78,9 @@ public class PlayerController : MonoBehaviour
     {
         _runSpeed = _walkSpeed * 2;
         _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
-        _socket.Connect(IPAddress.Parse("127.0.0.1"), 6666);
-        _sendPosition = new SyncInformation(9, 0, 0, 0);
-        _receivePosition = new SyncInformation(9, 0, 0, 0);
+        _socket.Connect(IPAddress.Parse(IP), Port);
+        _sendPosition = new SyncInformation(UninitializedPlayerIndex, 0, 0, 0);
+        _receivePosition = new SyncInformation(UninitializedPlayerIndex, 0, 0, 0);
     }
 
     // Update is called once per frame
@@ -134,7 +137,7 @@ public class PlayerController : MonoBehaviour
             _playerSpeed = _walkSpeed;
         }
 
-        if(Input.GetKey(KeyCode.A))
+        if(Input.GetKey(KeyCode.LeftArrow))
         {
             switch(_myPlayerIndex)
             {
@@ -153,7 +156,7 @@ public class PlayerController : MonoBehaviour
             }
             _isMoved = true;
         }
-        if(Input.GetKey(KeyCode.D))
+        if(Input.GetKey(KeyCode.RightArrow))
         {
             switch(_myPlayerIndex)
             {
@@ -172,7 +175,7 @@ public class PlayerController : MonoBehaviour
             }
             _isMoved = true;
         }
-        if(Input.GetKey(KeyCode.W))
+        if(Input.GetKey(KeyCode.UpArrow))
         {
             switch(_myPlayerIndex)
             {
@@ -191,7 +194,7 @@ public class PlayerController : MonoBehaviour
             }
             _isMoved = true;
         }
-        if(Input.GetKey(KeyCode.S))
+        if(Input.GetKey(KeyCode.DownArrow))
         {
             switch(_myPlayerIndex)
             {
@@ -224,11 +227,8 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("@@@@@@@@@@@@@ Receive Data From Server @@@@@@@@@@@@@@@@@@@@@@@");
             byte[] receivedData = new byte[16];
-            Debug.Log("11111111");
             _socket.Receive(receivedData, 16, SocketFlags.None);
-            Debug.Log("222222222");
             _receivePosition = (SyncInformation)ByteToStructure(receivedData, typeof(SyncInformation));
-            Debug.Log("receivedPosition x = " + _receivePosition.X);
             Vector3 vector3 = new Vector3(_receivePosition.X, _receivePosition.Y, _receivePosition.Z);
             Camera.PlayerList[_receivePosition.PlayerIndex].transform.Translate(vector3);
         }
