d6ad76078e8405ce67b82cf293c49f0c66311a7a
hyeonQyu <dhk0561@naver.com>
2020-03-26 15:35:49 +0900

    Add game launcher

--- Soccar/Assets/Scripts/Camera.cs
+++ Soccar/Assets/Scripts/Camera.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-
-    //private static GameObject[] _playerList;
-    //private static GameObject _player;
-    private int _playerIndex; // p1=0, p2=1, p3=2, p4=3
-
     private float _distance = 20f;
     private float _angle = 35f;
 
@@ -40,27 +35,11 @@ public class Camera : MonoBehaviour
 
     private const int PlayerCount = 4;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //_playerList = new GameObject[4];
-        //_playerList[0] = GameObject.Find("Player1");
-        //_playerList[1] = GameObject.Find("Player2");
-        //_playerList[2] = GameObject.Find("Player3");
-        //_playerList[3] = GameObject.Find("Player4");
-
-    }
-
     // Update is called once per frame
     void Update()
     {
-        if(PlayerController.IsConnected && _isFirstRun)
+        if(PlayerController.IsPlayerInitialized && _isFirstRun)
         {
-            // 플레이어 캐릭터 배정
-            //_playerIndex = PlayerController.PlayerIndex;
-            //_player = _playerList[_playerIndex];
-            //_player.GetComponent<PlayerInformation>().ID = ButtonControl.InputID.text;
-
             transform.eulerAngles = new Vector3(_angle, PlayerController.PlayerIndex * 90f, 0);
             _isFirstRun = false;
         }
@@ -76,23 +55,23 @@ public class Camera : MonoBehaviour
         // 해당하는 플레이어에 따라 카메라의 위치가 달라진다.
         /* 카메라는 플레이어로 부터 _distance만큼 떨어져 있으며(플레이어마다 X or Z) 높이(Y)는 _distance이다.
             또한 _angle 만큼 아래를 쳐다보고 있으며 해당 플레이어가 오른쪽으로 위치하도록 Rotation.Y 값을 조정한다. */
-        switch(_playerIndex)
+        switch(PlayerController.PlayerIndex)
         {
             case 0:
                 transform.transform.position = new Vector3(PlayerController.Player.transform.position.x, _distance, PlayerController.Player.transform.position.z - _distance);
-                transform.eulerAngles = new Vector3(_angle, _playerIndex * 90f, 0);
+                transform.eulerAngles = new Vector3(_angle, PlayerController.PlayerIndex * 90f, 0);
                 break;
             case 1:
                 transform.transform.position = new Vector3(PlayerController.Player.transform.position.x - _distance, _distance, PlayerController.Player.transform.position.z);
-                transform.eulerAngles = new Vector3(_angle, _playerIndex * 90f, 0);
+                transform.eulerAngles = new Vector3(_angle, PlayerController.PlayerIndex * 90f, 0);
                 break;
             case 2:
                 transform.transform.position = new Vector3(PlayerController.Player.transform.position.x, _distance, PlayerController.Player.transform.position.z + _distance);
-                transform.eulerAngles = new Vector3(_angle, _playerIndex * 90f, 0);
+                transform.eulerAngles = new Vector3(_angle, PlayerController.PlayerIndex * 90f, 0);
                 break;
             case 3:
                 transform.transform.position = new Vector3(PlayerController.Player.transform.position.x + _distance, _distance, PlayerController.Player.transform.position.z);
-                transform.eulerAngles = new Vector3(_angle, _playerIndex * 90f, 0);
+                transform.eulerAngles = new Vector3(_angle, PlayerController.PlayerIndex * 90f, 0);
                 break;
         }
     }
---
+++ Soccar/Assets/Scripts/GameLauncher.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLauncher : MonoBehaviour
+{
+    private static bool _isClickedStart;
+    public static bool IsClickedStart
+    {
+        set { _isClickedStart = value; }
+    }
+
+    [SerializeField]
+    GameObject _waitingText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerController.InitializePlayers();
+        NetworkManager.SetWebSocket();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_isClickedStart)
+        {
+            Debug.Log("Start button clicked");
+
+            // 게임 시작 버튼이 눌렸음을 서버에 전송
+            NetworkManager.Send("start_button", "start");
+
+            _isClickedStart = false;
+            return;
+        }
+
+        // 게임 시작 Ack를 맨 처음에 제대로 수신하면
+        if(NetworkManager.GameStart == "start" && PlayerController.PlayerIndex == 99)
+        {
+            // Player Index 요청
+            NetworkManager.Send("request_player_index", "req");
+
+            while(PlayerController.PlayerIndex == 99)
+            {
+                Debug.Log("==수신 대기중..."); // Player Index를 서버로부터 수신할 때까지 대기
+            }
+            Debug.Log("==인덱스 받음, 초기화 할차례");
+
+            // 플레이어 초기화
+            PlayerController.SetPlayer();
+            return;
+        }
+
+        PlayerController.KeyDowned();
+    }
+}
