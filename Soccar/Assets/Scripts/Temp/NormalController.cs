170c2b7ce9047c8315702df907184f87890f97d3
unknown <ryulurala@gmail.com>
2020-05-21 06:10:25 +0900

    Moving test & character animation

---
+++ Soccar/Assets/Scripts/Temp/NormalController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalController : MonoBehaviour
+{
+    Animator _animator;
+    public float MoveSpeed = 5f;
+    public float RotationSpeed = 360f;
+    CharacterController _characterCon;
+    // Start is called before the first frame update
+    void Start()
+    {    
+        _animator = GetComponent<Animator>();
+        _characterCon = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        // Smooth Move
+        if(direction.sqrMagnitude > 0.01f)
+        {
+            Vector3 forward = Vector3.Slerp(
+                transform.forward,
+                direction,
+                RotationSpeed * Time.deltaTime/Vector3.Angle(transform.forward, direction)
+            );
+            transform.LookAt(transform.position + forward);
+        }
+        _characterCon.Move(direction * MoveSpeed * Time.deltaTime);
+        _animator.SetFloat("speed", _characterCon.velocity.magnitude);
+        
+        if(Input.GetKey (KeyCode.Space))
+        {
+            _animator.SetTrigger("isJump");
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            _animator.SetTrigger("isShoot");
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            _animator.SetTrigger("isTackle");
+        }  
+    }
+}
---
+++ Soccar/Assets/Scripts/Temp/RagDollController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollContorller : MonoBehaviour
+{
+    Rigidbody _rigidBody;
+
+    private float _vertical;
+    private float _horizontal;
+    private float _verticalRaw;
+    private float _horizontalRaw;
+    Vector3 targetRotation;
+    public float RotationSpeed = 10;
+    public float Speed = 100;
+    Animator _animator;
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        // Direction Axis
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+
+        _horizontalRaw = Input.GetAxisRaw("Horizontal");
+        _verticalRaw = Input.GetAxisRaw("Vertical");
+
+        Vector3 input = new Vector3(_horizontal, 0, _vertical);
+        Vector3 inputRaw = new Vector3(_horizontalRaw, 0, _verticalRaw);
+
+        if(input.sqrMagnitude>1f)
+        {
+            input.Normalize();
+        }
+        
+        if(inputRaw!=Vector3.zero)
+        {
+            targetRotation = Quaternion.LookRotation(input).eulerAngles;
+        }
+        _rigidBody.rotation = Quaternion.Slerp(
+            transform.rotation,
+            Quaternion.Euler(targetRotation.x, Mathf.Round(targetRotation.y/45)*45, targetRotation.z),
+            Time.deltaTime*RotationSpeed
+            );
+        
+        if(inputRaw.sqrMagnitude!=0)
+        {
+            _animator.enabled = true;
+        }
+        else if(inputRaw.sqrMagnitude == 0)
+        {
+            _animator.enabled = false;
+        }
+    }
+}
