041cabb7ae8fdd5cd6b88f957a34e9baa2a92852
hyeonQyu <dhk0561@naver.com>
2020-05-30 20:58:10 +0900

    Load game scene when click game start button and make NetworkManager not static

---
+++ Soccar/Assets/Scripts/Network/SceneMedium.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneMedium : MonoBehaviour
+{
+    public int Port { get; set; }
+    public int Headcount { get; set; }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
