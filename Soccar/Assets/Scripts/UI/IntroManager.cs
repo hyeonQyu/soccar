13e2284ee4244339cd7882962f706783dfd28d43
hyeonQyu <dhk0561@naver.com>
2020-06-04 14:29:57 +0900

    Change logo and add intro scene

---
+++ Soccar/Assets/Scripts/UI/IntroManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class IntroManager : MonoBehaviour
+{
+    [SerializeField]
+    private Image _logoPanel;
+    [SerializeField]
+    private Image _mainScreenPanel;
+    [SerializeField]
+    private Image _pressAnyKey;
+
+    public static bool IsLogoDestroyed { get; set; }
+    public static bool IsMainDestroyed { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator logoAnimator = _logoPanel.GetComponent<Animator>();
+        logoAnimator.SetBool("isDestroy", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 아무 키나 누르면 Main Screen이 사라지고 로그인 화면으로 이동
+        if(IsLogoDestroyed && Input.anyKeyDown)
+        {
+            Destroy(_pressAnyKey.gameObject);
+            Animator mainScreenAnimator = _mainScreenPanel.GetComponent<Animator>();
+            mainScreenAnimator.SetBool("isDestroy", true);
+        }
+
+        if(IsMainDestroyed)
+            SceneManager.LoadScene("Lobby Scene");
+    }
+}
