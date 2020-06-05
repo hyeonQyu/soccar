c199551c892f3b63d2c8ce071f25b84fca8fa480
hyeonQyu <dhk0561@naver.com>
2020-06-05 21:50:56 +0900

    Add audio sources: Kick Ball, Hit Post, Crowd Goal, Crowd Default

--- Soccar/Assets/Scripts/Common/Sound.cs
+++ Soccar/Assets/Scripts/Common/Sound.cs
@@ -7,7 +7,11 @@ public class Sound
     private GameObject _sounds;
 
     // 게임
+    public AudioSource CrowdDefault { get; private set; }
+    public AudioSource CrowdGoal { get; private set; }
     public AudioSource Whistle { get; private set; }
+    public AudioSource KickBall { get; private set; }
+    public AudioSource HitPost { get; private set; }
 
     // 로비
 
@@ -18,8 +22,12 @@ public class Sound
 
         // 게임
         if(isGameScene)
-        {    
+        {
+            CrowdDefault = _sounds.transform.Find("Crowd Default").GetComponent<AudioSource>();
+            CrowdGoal = _sounds.transform.Find("Crowd Goal").GetComponent<AudioSource>();
             Whistle = _sounds.transform.Find("Whistle").GetComponent<AudioSource>();
+            KickBall = _sounds.transform.Find("Kick Ball").GetComponent<AudioSource>();
+            HitPost = _sounds.transform.Find("Hit Post").GetComponent<AudioSource>();
         }
         // 로비
         else
