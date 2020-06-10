c28eacc01a35406fd8e2aedbba7fab5f81d69ad1
unknown <ryulurala@gmail.com>
2020-06-11 02:24:57 +0900

    BallController velocity * Time.fixedDeltaTime

--- Soccar/Assets/Scripts/Game/BallController.cs
+++ Soccar/Assets/Scripts/Game/BallController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallController:MonoBehaviour
+public class BallController : MonoBehaviour
 {
     private Animator _collisionAnimator;
     private Collider _ball;
@@ -32,7 +32,7 @@ public class BallController:MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Net"))
+        if (other.gameObject.CompareTag("Net"))
         {
             _ball.material.bounciness *= 0.01f;
         }
@@ -44,20 +44,20 @@ public class BallController:MonoBehaviour
         GameObject other = collision.gameObject;
 
         // 땅에 부딪히면 탄성 감소
-        if(other.CompareTag("Ground"))
+        if (other.CompareTag("Ground"))
         {
             _ball.material.bounciness *= 0.8f;
             return;
         }
         // 그물은 공을 튀기지 않기 때문에 그물 위에 올라가면 공을 내려줘야 함
-        else if(other.CompareTag("Net"))
+        else if (other.CompareTag("Net"))
         {
             _ball.material.bounciness *= 0.01f;
             _isOnNet = true;
             StartCoroutine(MoveBallOnNet());
             return;
         }
-        else if(other.CompareTag("Fense"))
+        else if (other.CompareTag("Fense"))
         {
             _ball.material.bounciness = 1;
         }
@@ -67,7 +67,7 @@ public class BallController:MonoBehaviour
 
         _isShoot = false;
 
-        if(LayerMask.LayerToName(other.layer).Equals("RagDoll"))
+        if (LayerMask.LayerToName(other.layer).Equals("RagDoll"))
         {
             // 마지막 사용자 기록
             _lastPlayer = collision.transform.root.gameObject;
@@ -76,38 +76,39 @@ public class BallController:MonoBehaviour
             _collisionAnimator = _lastPlayer.transform.GetChild(0).gameObject.GetComponent<Animator>();
 
             // 슈팅
-            if(_collisionAnimator.GetCurrentAnimatorStateInfo(0).fullPathHash == PlayerController.Hash.Shoot && collision.gameObject.name.Equals("RightLeg"))
+            if (_collisionAnimator.GetCurrentAnimatorStateInfo(0).fullPathHash == PlayerController.Hash.Shoot && collision.gameObject.name.Equals("RightLeg"))
             {
                 _isShoot = true;
             }
         }
     }
 
-    private void OnCollisionStay(Collision collision) 
+    private void OnCollisionStay(Collision collision)
     {
-        if(LayerMask.LayerToName(collision.gameObject.layer).Equals("RagDoll"))
+        if (LayerMask.LayerToName(collision.gameObject.layer).Equals("RagDoll"))
         {
             // 드리블
-           _rigidBody.velocity = collision.transform.forward * _collisionAnimator.GetFloat("SpeedFloat") * 5.0f;
+            _rigidBody.velocity = collision.transform.forward * _collisionAnimator.GetFloat("SpeedFloat") * 5.0f;
         }
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Net"))
+        if (collision.gameObject.CompareTag("Net"))
         {
             _isOnNet = false;
         }
 
-        if( LayerMask.LayerToName(collision.gameObject.layer).Equals("RagDoll") && _isShoot)
+        if (LayerMask.LayerToName(collision.gameObject.layer).Equals("RagDoll") && _isShoot)
         {
             // 방향은 Avatar가 바라보는 방향
             Vector3 direction = collision.transform.root.GetChild(0).forward;
             // Debug.Log("Shoot!!");
             // 한 번만 실행하도록 위치 바꿔줌
             transform.position += direction * 0.5f;
-            _rigidBody.velocity = direction * Input.GetAxis("Sensitivity") * _shootSpeed + new Vector3(0, 3.0f, 0);
-            
+
+            // 살짝 위로 올라가도록
+            _rigidBody.velocity = direction * Input.GetAxis("Sensitivity") * _shootSpeed + new Vector3(0, 3.0f, 0) * Time.fixedDeltaTime;
         }
 
     }
@@ -116,14 +117,14 @@ public class BallController:MonoBehaviour
     {
         int loop = 0;
         // 4초간 대기하며 공이 그물 위에 있는지 검사
-        while(_isOnNet && loop < 8)
+        while (_isOnNet && loop < 8)
         {
             yield return new WaitForSeconds(0.5f);
             loop++;
         }
 
         // 공이 4초 후에도 그물 위에 있으면 탄성 복구 후 중앙으로 공 이동
-        if(_isOnNet)
+        if (_isOnNet)
         {
             _ball.material.bounciness = 0.8f;
             transform.position = new Vector3(0, 10, 0);
--- Soccar/Assets/Scripts/Game/TackleEvent.cs
+++ Soccar/Assets/Scripts/Game/TackleEvent.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class TackleEvent : MonoBehaviour
 {
     private Animator _animator;
+    private float _tackleForce;
     // Start is called before the first frame update
     void Start()
     {
+        _tackleForce = 5.0f;
         _animator = transform.root.GetChild(0).gameObject.GetComponent<Animator>();
     }
     private void OnCollisionEnter(Collision other)
     {
-        if(transform.root.gameObject.GetInstanceID() != other.transform.root.gameObject.GetInstanceID() )
+        if (_animator.GetCurrentAnimatorStateInfo(0).fullPathHash == Animator.StringToHash("Base Layer.Tackle"))
         {
-            if(_animator.GetCurrentAnimatorStateInfo(0).fullPathHash == Animator.StringToHash("Base Layer.Tackle") && LayerMask.LayerToName(other.gameObject.layer).Equals("RagDoll"))
+            if (transform.root.gameObject.GetInstanceID() != other.transform.root.gameObject.GetInstanceID())
             {
-                Debug.Log("태클 당하는 놈" + other.transform.root.gameObject.name);
-                other.rigidbody.AddForce(new Vector3(0, 2000.0f, 0), ForceMode.Impulse);    // 수정해야함
+                Debug.Log("충돌 당하는 놈 " + other.gameObject.name);
+                if (LayerMask.LayerToName(other.gameObject.layer).Equals("RagDoll") && other.gameObject.name.Equals("RightLeg"))
+                {
+                    Debug.Log("태클 당하는 놈" + other.transform.root.gameObject.name);
+                    Vector3 ss = new Vector3(0, 1, 0);
+                    Debug.Log("속도no = " + ss * _tackleForce);
+                    Debug.Log("속도 = " + ss * _tackleForce * Time.fixedDeltaTime);
+                    other.transform.root.GetChild(1).GetChild(0).GetChild(0).position += Vector3.up * _tackleForce * Time.fixedDeltaTime;  // 수정해야함
+                }
             }
         }
     }
