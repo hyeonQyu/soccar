cce4d53a5538bfae26267f3755cbeed3e7bdfecb
unknown <ryulurala@gmail.com>
2020-06-10 16:45:47 +0900

    NULL

--- Soccar/Assets/Scripts/Game/TackleEvent.cs
+++ Soccar/Assets/Scripts/Game/TackleEvent.cs
@@ -14,11 +14,12 @@ public class TackleEvent : MonoBehaviour
     {
         if(transform.root.gameObject.GetInstanceID() != other.transform.root.gameObject.GetInstanceID())
         {
-            if(_animator.GetCurrentAnimatorStateInfo(0).nameHash == Animator.StringToHash("Base Layer.Tackle") && LayerMask.LayerToName(other.gameObject.layer).Equals("RagDoll"))
+            if(_animator.GetCurrentAnimatorStateInfo(0).fullPathHash == Animator.StringToHash("Base Layer.Tackle") && LayerMask.LayerToName(other.gameObject.layer).Equals("RagDoll"))
             {
                 Debug.Log("태클 당하는 놈" + other.transform.root.gameObject.name);
                 other.rigidbody.AddForce(new Vector3(0, 2000.0f, 0), ForceMode.Impulse);
             }
-        } 
+            
+        }
     }
 }
