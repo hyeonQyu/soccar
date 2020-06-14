65eac4cea112b5cd0ee5a68228ac2baa87e8cc33
unknown <ryulurala@gmail.com>
2020-06-14 16:40:47 +0900

    Modify FeverBall collision script

--- Soccar/Assets/Scripts/Game/FeverBall.cs
+++ Soccar/Assets/Scripts/Game/FeverBall.cs
@@ -7,21 +7,24 @@ public class FeverBall : MonoBehaviour
     private GameObject _followYellowLight;
     private GameObject _star;
     private GameObject _explosion;
+    private bool _isFinish;
     // Start is called before the first frame update
     void Start()
     {
+        _isFinish = false;
         _followYellowLight = transform.GetChild(0).gameObject;
         _star = transform.GetChild(1).gameObject;
         _explosion = transform.GetChild(2).gameObject;
     }
-    
+
     private void OnCollisionEnter(Collision other)
     {
-        if(other.gameObject.tag.Equals("Ground"))
+        if (!_isFinish)
         {
             _explosion.SetActive(true);
             _followYellowLight.SetActive(false);
             _star.SetActive(false);
+            _isFinish = true;
         }
     }
 }
