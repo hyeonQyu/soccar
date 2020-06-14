ef607bb26573b0c1621202c709ffc8a33f6e7e5e
unknown <ryulurala@gmail.com>
2020-06-14 16:28:35 +0900

    Create FeverBall Prefab

---
+++ Soccar/Assets/Scripts/Game/FeverBall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeverBall : MonoBehaviour
+{
+    private GameObject _followYellowLight;
+    private GameObject _star;
+    private GameObject _explosion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _followYellowLight = transform.GetChild(0).gameObject;
+        _star = transform.GetChild(1).gameObject;
+        _explosion = transform.GetChild(2).gameObject;
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag.Equals("Ground"))
+        {
+            _explosion.SetActive(true);
+            _followYellowLight.SetActive(false);
+            _star.SetActive(false);
+        }
+    }
+}
